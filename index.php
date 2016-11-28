04c8d1716c17a5f84cff7e0519e3e842eb5f0030
rajasj08 <rajasj08@gmail.com>
2016-11-28 07:54:46 +0530

    still struggling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hi Rajesh Eb Deploy success from GitHub</p>'; ?> 
+    <?php // echo '<p>Hi Rajesh Eb Deploy success from GitHub</p>'; ?> 
+    <?php echo '<p>Still strugling</p>'; ?> 
   </body>
 </html>
