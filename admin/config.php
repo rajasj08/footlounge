f712bac98d0b2814c99636e5328bdfa5c9ffc7c3
rajasj08 <rajasj08@gmail.com>
2016-11-28 12:43:39 +0530

    OC installed

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/GitHub/footlounge/admin/');
+define('HTTP_CATALOG', 'http://localhost/GitHub/footlounge/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/GitHub/footlounge/admin/');
+define('HTTPS_CATALOG', 'http://localhost/GitHub/footlounge/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/wamp64/www/GitHub/footlounge/admin/');
+define('DIR_SYSTEM', 'E:/wamp64/www/GitHub/footlounge/system/');
+define('DIR_IMAGE', 'E:/wamp64/www/GitHub/footlounge/image/');
+define('DIR_LANGUAGE', 'E:/wamp64/www/GitHub/footlounge/admin/language/');
+define('DIR_TEMPLATE', 'E:/wamp64/www/GitHub/footlounge/admin/view/template/');
+define('DIR_CONFIG', 'E:/wamp64/www/GitHub/footlounge/system/config/');
+define('DIR_CACHE', 'E:/wamp64/www/GitHub/footlounge/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/wamp64/www/GitHub/footlounge/system/storage/download/');
+define('DIR_LOGS', 'E:/wamp64/www/GitHub/footlounge/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/wamp64/www/GitHub/footlounge/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/wamp64/www/GitHub/footlounge/system/storage/upload/');
+define('DIR_CATALOG', 'E:/wamp64/www/GitHub/footlounge/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'footlounge_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
