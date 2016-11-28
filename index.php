67824b54d766d25974b7b769db167342123eb36b
rajasj08 <rajasj08@gmail.com>
2016-11-28 09:01:43 +0530

    open cart default framework

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hi Rajesh Eb Deploy success from GitHub</p>'; ?> 
-    <?php //echo '<p>Still strugling</p>'; ?> 
-  </body>
-</html>
+<?php
+// Version
+define('VERSION', '2.3.0.2');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
